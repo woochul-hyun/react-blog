a13c89f45a7d33b54b527035c52ae6f4c10dd88e
Woochul Hyun <woochul.hyun91@gmail.com>
2019-09-22 19:53:31 +0900

    auto template

--- blog-frontend/src/pages/RegisterPage.js
+++ blog-frontend/src/pages/RegisterPage.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import AuthTemplate from '../components/auth/AuthTemplate';
+import AuthForm from '../components/auth/AuthForm';
 
 const RegisterPage = () => {
-  return <div>Sign up</div>;
+  return (
+    <AuthTemplate>
+      <AuthForm />
+    </AuthTemplate>
+  );
 };
 
 export default RegisterPage;
